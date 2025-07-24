5285117dda5f09b338fd11fb477fb0540d7326ef
Orlando <nantenainaorlando1@gmail.com>
2025-07-24 04:12:30 +0100

    Ajout des

--- src/composants/Navbar.jsx
+++ src/composants/Navbar.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return <>
    <nav className='flex items-center justify-between pl-8 pr-8 font-bold '>
-    <div className='flex items-center mr-20'>
+    <div className='bar flex items-center mr-20'>
         <img className='w-16 h-16 rounded-full ' src='/images/Mockup_InfoCadi.jpg'/>
         <h1 className='text-blue-700 font-bold'>TechStore</h1>
     </div>
-    <Link to="/" className='cursor-pointer hover:text-blue-600'>Accueil</Link>
-    <Link to="produits" className='cursor-pointer hover:text-blue-600'>Produits</Link>
+    <Link to="/" className='lnk cursor-pointer hover:text-blue-600 '>Accueil</Link>
+    <Link to="produits" className=' lnk cursor-pointer hover:text-blue-600 '>Produits</Link>
     <img className='w-14 h-13' src='/images/panier.png'/>
     <img className='w-16 h-16 rounded-full' src='images/admin.png'/>
    </nav>
--- src/composants/ProduitCard.jsx
+++ src/composants/ProduitCard.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import { products } from '../data/produits'
 export default function ProduitCard() {
   return (
-    <div>ProduitCard</div>
+    <>
+     <div className='card_principale'>
+        {products.map((produit)=>(
+            <div className="card bg-base-100 w-96 shadow-sm h-100 m-3 pl-2" key={produit.id}>
+                <figure>
+                    <img className='object-cover' src={produit.image}/>
+                </figure>
+                <h1 className="card-title">{produit.title}</h1>
+                <p>{produit.category}</p>
+                <div className="card-actions justify-end">
+                    <button className='btn btn-primary mb-2 mr-2'>Details</button>
+                    <button className='btn btn-primary mb-2 mr-2'>panier</button>
+                </div>
+            </div>
+        ))
+        }
+
+     </div>
+    </>
   )
 }
